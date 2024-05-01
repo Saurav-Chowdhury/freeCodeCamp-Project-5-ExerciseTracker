ab7a69ffaeeeea1c3ef867f6b66186e73e88b156
Saurav Chowdhury <85385358+Saurav-Chowdhury@users.noreply.github.com>
2024-05-01 17:03:35 +0530

    Some progress

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+
+const mongoose=require('mongoose');
+const bodyParser=require('body-parser');
+const schema=mongoose.Schema;
+
+app.use(bodyParser.json())
+
+try{
+  mongoose.connect(process.env.MONGO_URI);
+  console.log('Connected to DB');
+}catch(err){
+console.log(err);
+}
+
+//UserSchema
+
+const userSchema=new schema({
+  username: {
+    type: String,
+    required: true
+  }
+})
+
+const User=mongoose.model('Users',userSchema)
+
 
 app.use(cors())
 app.use(express.static('public'))
@@ -10,7 +35,29 @@ app.get('/', (req, res) => {
 });
 
 
+//solution
 
+app.post('/api/users',async (req,res)=>{
+  let username=req.body.username;
+
+  const userObj=new User({
+    username
+  });
+
+  const data=await userObj.save();
+  res.json({
+    username: data.username,
+    _id: data._id
+  })
+})
+
+app.get('/api/users',async (req,res)=>{
+  const data=await User.find({},{username: true, _id: true})
+  res.json(data)
+
+})
+
+//
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
